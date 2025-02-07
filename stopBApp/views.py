9565a09f11c5795cad38c908215fd449d4422045
sania <tariqs@uwm.edu>
2025-02-06 21:01:58 -0600

    Created a view and configured url for homepage. Currently showing an error due to template not existing.

--- stopBApp/views.py
+++ stopBApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class HomepageView(View):
+    def get(self, request):
+        if request.method == 'GET':
+            return render(request, "homepage-view-template.html")
+
+    # def post(self, request):
+    #     if request.method == 'POST':
+    #
