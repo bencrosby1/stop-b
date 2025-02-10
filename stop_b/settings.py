2a4cb8ec9baf854aacab6257b1c3e099f050fde1
bencrosby1 <bwcrosby@uwm.edu>
2025-02-10 17:46:09 -0600

    adding base template directory to settings.py, file structure changes

--- stop_b/settings.py
+++ stop_b/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'stop_b.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'stopBApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
