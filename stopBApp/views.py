ce0085314e60726efb2c84c591e5450ac3ad903e
bencrosby1 <bwcrosby@uwm.edu>
2025-02-13 17:36:07 -0600

    updating views.py with correct url

--- stopBApp/views.py
+++ stopBApp/views.py
@@ -5,7 +5,7 @@ from django.views import View
 class HomepageView(View):
     def get(self, request):
         if request.method == 'GET':
-            return render(request, "homepage-view-template.html")
+            return render(request, "home.html")
 
     # def post(self, request):
     #     if request.method == 'POST':
