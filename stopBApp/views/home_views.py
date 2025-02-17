d00837e11963acd0567ecb95a1f9c514dc4f053c
bencrosby1 <bwcrosby@uwm.edu>
2025-02-17 14:35:32 -0600

    creating individual views files, requires adding __init__ file to do the imports

--- stopBApp/views/home_views.py
+++ stopBApp/views/home_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-class HomepageView(View):
+class Home(View):
     def get(self, request):
         if request.method == 'GET':
             return render(request, "home.html")
