4cb295e008560d9817f72e7b7cd0d18be15362fe
sania <tariqs@uwm.edu>
2025-02-27 19:04:35 -0600

    Geolocation fetching using JS. Created a model to save location. template for nearby bus stops, changed urls and created a view for

--- stopBApp/models.py
+++ stopBApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import requests
 
 # Create your models here.
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return f"Latitude: {self.latitude}, Longitude: {self.longitude}"
--- stopBApp/views/__init__.py
+++ stopBApp/views/__init__.py
@@ -1,3 +1,5 @@
 from .home_views import Home
 from .login_views import Login
 from .register_views import Register
+from .bustime_view import Bustime
+from .stopnav_view import location
