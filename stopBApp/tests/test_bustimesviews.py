bd7bcab2a44d5e6fd78e51cfc42d84c62890696f
trex821 <39505785+trex821@users.noreply.github.com>
2025-04-10 19:08:25 -0500

    Acceptance tests for bus time views

---
+++ stopBApp/tests/test_bustimesviews.py
@@ -0,0 +1,36 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class BusTimesTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_bus_time_view_status_code(self):
+        response = self.client.get(reverse('bus_times'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_bus_times_view_valid(self):
+        valid_stop_id = 1718
+        response = self.client.get(reverse('bus_times', args={'stop_id': valid_stop_id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_bus_times_view_invalid_stop(self):
+        invalid_stop_id = -1
+        response = self.client.get(reverse('bus_times', args={'stop_id': invalid_stop_id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(isinstance(response.json(), dict))
+
+    def test_bus_times_view_coordinates_valid(self):
+        stop_lat = 43.0166003
+        stop_lon = -88.0382036
+        response = self.client.get(reverse('bus_times', args={'lat': stop_lat, 'lon': stop_lon}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_bus_times_view_coordinates_invalid(self):
+        stop_lat = 1234567890
+        stop_lon = 1234567890
+        response = self.client.get(reverse('bus_times', args={'lat': stop_lat, 'lon': stop_lon}))
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(isinstance(response.json(), dict))
