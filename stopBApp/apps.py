4e89f151cd0102d844fb28b498637da5d6e084d0
youngc311 <youngc311@gmail.com>
2025-04-24 19:11:43 -0500

    Latest version
    Detours list being pulled
    Line saving working

--- stopBApp/apps.py
+++ stopBApp/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError
+from django.core.management import call_command
 
 class StopbappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stopBApp'
+
+    def ready(self):
+        from stopBApp.utils import sync_bus_lines
+        try:
+            call_command('check')
+            sync_bus_lines()
+        except OperationalError:
+            print("Database is not ready yet. Skipping bus line sync.")
